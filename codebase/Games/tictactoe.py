5dccaf2e783609f49463b65029fd5f7f375e690c
TylersDurden <srobbin1@ithaca.edu>
2019-06-08 11:43:16 -0400

    updates

--- codebase/Games/tictactoe.py
+++ codebase/Games/tictactoe.py
@@ -1,5 +1,5 @@
 import numpy as np
-import sys
+import time
 
 
 class Board:
@@ -32,8 +32,41 @@ class Board:
         return blank, xs, os
 
     def set_state(self, pos, mov):
-        if mov >=0 and pos[0] <= 2 and pos[1] <= 2:
+        moves = [0, 1]
+        if mov in moves and self.state[pos[0], pos[1]] == -1:
             self.state[pos[0], pos[1]] = mov
+            return True
+        else:
+           return False
+
+    def isComplete(self):
+        finished = False
+        row1 = self.state[0, :]
+        row2 = self.state[1, :]
+        row3 = self.state[2, :]
+
+        col1 = self.state[:,0]
+        col2 = self.state[:,1]
+        col3 = self.state[:,2]
+
+        dag1 = [self.state[0,0], self.state[1,1], self.state[2,2]]
+        dag2 = [self.state[0,2], self.state[1,1], self.state[2,0]]
+
+        # BOOLEANS
+        r1 = len(np.unique(row1)) == 1 and -1 not in row1
+        r2 = len(np.unique(row2)) == 1 and -1 not in row2
+        r3 = len(np.unique(row3)) == 1 and -1 not in row3
+
+        c1 = len(np.unique(col1)) == 1 and -1 not in col1
+        c2 = len(np.unique(col2)) == 1 and -1 not in col2
+        c3 = len(np.unique(col3)) == 1 and -1 not in col3
+
+        d1 = len(np.unique(dag1)) == 1 and -1 not in dag1
+        d2 = len(np.unique(dag2)) == 1 and -1 not in dag2
+        # Check IF ANY conditions for completion were met
+        if r1 or r2 or r3 or c1 or c2 or c3 or d1 or d2:
+            finished = True
+        return finished
 
 
 class Player:
@@ -46,11 +79,14 @@ class Player:
         self.board = state
 
     def random_move(self, board):
-        x = np.random.random_integers(0, 2, 1)[0]
-        y = np.random.random_integers(0, 2, 1)[0]
-        val = np.random.random_integers(0, 1, 1)[0]
-        board.set_state([x, y], val)
-        self.board = board.state
+        moved = False
+        while not moved:
+            x = np.random.random_integers(0, 2, 1)[0]
+            y = np.random.random_integers(0, 2, 1)[0]
+            val = np.random.random_integers(0, 1, 1)[0]
+            moved = board.set_state([x, y], val)
+            self.board = board.state
+
         return board
 
     def interactive_move(self, board):
@@ -65,6 +101,7 @@ class Player:
         return board
 
 
+
 board = Board()
 player1 = Player(board)
 robot = Player(board)
@@ -72,8 +109,18 @@ robot = Player(board)
 blank, Xs, Os = board.blank_squares()
 
 while board.blank_squares()[0] > 0:
+    t0 = time.time()
     board = robot.random_move(board)
+    if board.isComplete():
+        print 'Robot Wins!'
+        board.show_board()
+        exit(0)
+
     board.show_board()
     board = player1.interactive_move(board)
+    if board.isComplete():
+        print 'Player 1 Wins!'
+        board.show_board()
+        exit(0)
     print '-------------------------------------'
 
